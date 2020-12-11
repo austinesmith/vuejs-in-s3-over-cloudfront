43198cc8a4f8c1f9a17bd7c5caed00a65116c54d
Austin Smith <austinfosec@gmail.com>
2020-12-11 11:31:49 -0600

    sample vuetify 1

--- packages/vuejs-app/src/App.vue
+++ packages/vuejs-app/src/App.vue
@@ -1,49 +1,36 @@
 <template>
-  <div id="app" style="max-width:750px; margin:0 auto;">
-    <img alt="Amazon Web Services logo" src="./assets/AWS.jpg" class="responsiveaws"><br/>
-    <img alt="Cloud Development Kit logo" src="./assets/cdk.png" class="responsive">
-    <img alt="Typescript logo" src="./assets/ts.png" class="responsive">
-    <img alt="Vue logo" src="./assets/vue.png" class="responsive">
-    <img alt="Simple Storage Service logo" src="./assets/s3.png" class="responsive">
-    <img alt="Yarn logo" src="./assets/yarn.jpg" class="responsive">
-    <img alt="Git logo" src="./assets/git.png" class="responsive">
-    <img alt="Visual Studio Code logo" src="./assets/vsc.png" class="responsive">
-    <img alt="Github logo" src="./assets/github.png" class="responsive">
-    <img alt="CloudFormation logo" src="./assets/cfn.png" class="responsive"><br/><br/>
-    
-
-    <HelloWorld msg="This App Was Deployed Using AWS CDK"/>
-  </div>
+  <v-app>
+    dark
+    <v-app-bar
+      app
+      flat
+      dark
+    >
+      <Header/>
+    </v-app-bar>
+    <v-content>
+      <Carousel/>
+    </v-content>
+    <v-main>
+      
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header';
+import Carousel from './components/Carousel';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 20px;
-}
-.responsive {
-  width: 12%;
-  height: auto;
-  padding: 15px 15px 15px 15px;
-}
-.responsiveaws {
-  width: 55%;
-  height: auto;
-  padding-bottom: 40px;
-}
-</style>
+  components: {
+    Header,
+    Carousel
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ packages/vuejs-app/src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+        <div style="flex-basis: 20%">
+          <v-toolbar dark>
+
+            <v-toolbar-title>Austin Smith Portfolio</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-toolbar-items>
+
+            <v-btn text>
+              HOME
+            </v-btn>
+
+            <v-btn text>
+              ABOUT
+            </v-btn>
+
+            <v-btn text>
+              CONTACT
+            </v-btn>
+
+            </v-toolbar-items>
+          </v-toolbar>
+          
+          </div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+
+    data: () => ({
+    }),
+  }
+</script>
