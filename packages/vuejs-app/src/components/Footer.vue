080a8ae87e08021e378fbe607b5ffacf7640119e
Austin Smith <austinfosec@gmail.com>
2021-01-26 00:27:47 -0600

    footer text grey

--- packages/vuejs-app/src/components/Footer.vue
+++ packages/vuejs-app/src/components/Footer.vue
@@ -64,21 +64,22 @@
 
       </v-card-text>
 
-      <v-card-text class="white--text pt-0">
+      <v-card-text class="grey--text pt-0">
         This web SPA was created with Vuetify, a JavaScript component manager powered by Vue.js. 
         It uses the AWS Cloud Development Kit with TypeScript for automated deployment to Amazon Web Services. 
         The CDK automatically provisions an S3 bucket for static hosting behind the CloudFront Content Delivery Network.
         It uses Yarn as a package manager and Yarn Workspaces to create a monolithic repository. 
-        Changes are tracked by Git and Github Actions are used for CI/CD. <a href=https://github.com/austinesmith/vuejs-in-s3-over-cloudfront>[View: This Repository]</a><br>
+        Changes are tracked by Git and Github Actions are used for CI/CD.<br> <a href=https://github.com/austinesmith/vuejs-in-s3-over-cloudfront>[View: This Repository]</a><br>
         
         
       </v-card-text>
 
       <v-divider></v-divider>
 
-      <v-card-text class="white--text">
+      <v-card-text class="grey--text">
         
-        <strong>Created by Austin Smith</strong> — <a href="https://opensource.org/licenses/ISC">ISC</a> {{ new Date().getFullYear() }}<br><br>
+        <strong class="white--text">Created by Austin Smith</strong> — <a href="https://opensource.org/licenses/ISC">ISC</a> {{ new Date().getFullYear() }}<br><br>
+        powered by<br>
         <img src="../assets/carousel-images/awswhite.png" width="100">
         
       </v-card-text>
