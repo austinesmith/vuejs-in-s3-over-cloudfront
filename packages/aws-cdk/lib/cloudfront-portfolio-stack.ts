354633e7b070ad58fd9b91bc8d35c806c7539cae
Austin Smith <austinfosec@gmail.com>
2021-03-20 03:12:22 -0500

    clean up code

--- packages/aws-cdk/lib/cloudfront-portfolio-stack.ts
+++ packages/aws-cdk/lib/cloudfront-portfolio-stack.ts
@@ -3,32 +3,27 @@ import * as s3 from '@aws-cdk/aws-s3';
 import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
 import { CfnOutput } from '@aws-cdk/core';
 import * as cloudfront from '@aws-cdk/aws-cloudfront';
-// install cmd: 'npm i @aws-cdk/aws-s3-deployment'
 
+// create stack
 export class CloudfrontPortfolioStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
     // create bucket
     const myBucket = new s3.Bucket(this, "WebsiteBucketForCloudfrontPortfolio", {
-      // allow bucket to access to public internet
       publicReadAccess: true,
-      // switched RemovalPolicy.RETAIN retains the bucket in s3
       removalPolicy: cdk.RemovalPolicy.RETAIN,
       websiteIndexDocument: "index.html",
       websiteErrorDocument: "index.html"
     });
 
-    // create deployment resource
-    // constructs new instance of BucketDeployment class
+    // create bucket deployment resource
     const deployment = new s3Deployment.BucketDeployment(this, "deployStaticWebsite", {
-      // directory of the web asset
       sources: [s3Deployment.Source.asset("../vuejs-app/dist")],
-      // (experimental) The S3 bucket to sync the contents of the zip file to.)
       destinationBucket: myBucket
    });
 
+   // create cloudfront distribution
    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'cdk-example-cfront', {
     originConfigs: [
       {
@@ -38,21 +33,7 @@ export class CloudfrontPortfolioStack extends cdk.Stack {
         behaviors : [ {isDefaultBehavior: true}]
       }
     ],
-//    aliasConfiguration: {
-//      acmCertRef: certificateArn,
-//      names: ['cdk-example.awsexamples.dev']
-//    }
   });
-
-//   const myDistribution = new cloudfront.Distribution(this, "cdk-cloudfront", {
-//    originConfigs: [
-//      s3OriginSource: {
-//        s3BucketSource: myBucket
-//      },
-//      behaviors : [ {isDefaultBucket: true}]
-//    }
-//    ]
-//   });
 
    // return for website URL
    new CfnOutput(this, "URL", {
@@ -60,6 +41,4 @@ export class CloudfrontPortfolioStack extends cdk.Stack {
      value: myBucket.bucketWebsiteUrl
    });
   }
-
-
 }
